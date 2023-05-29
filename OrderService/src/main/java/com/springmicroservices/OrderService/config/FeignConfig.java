12f2cf5b007262e36b100d09b0630ab9eefc5bfa
prudhvi-nakkina <nakkina.p@northeastern.edu>
2023-05-28 20:27:01 -0400

    implemented feign errordecoder for custom handling

---
+++ OrderService/src/main/java/com/springmicroservices/OrderService/config/FeignConfig.java
@@ -0,0 +1,15 @@
+package com.springmicroservices.OrderService.config;
+
+import com.springmicroservices.OrderService.external.decoder.CustomErrorDecoder;
+import feign.codec.ErrorDecoder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class FeignConfig {
+
+    @Bean
+    ErrorDecoder errorDecoder() {
+        return new CustomErrorDecoder();
+    }
+}
