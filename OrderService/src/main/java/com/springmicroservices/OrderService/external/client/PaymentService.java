8ccdb98ed07ccac0b833c83ca0b5eeb991c7be8b
prudhvi-nakkina <nakkina.p@northeastern.edu>
2023-05-30 03:28:28 -0400

    Implemented Circuit Breaker in Order Service

--- OrderService/src/main/java/com/springmicroservices/OrderService/external/client/PaymentService.java
+++ OrderService/src/main/java/com/springmicroservices/OrderService/external/client/PaymentService.java
@@ -1,14 +1,21 @@
 package com.springmicroservices.OrderService.external.client;
 
+import com.springmicroservices.OrderService.exception.CustomException;
 import com.springmicroservices.OrderService.external.Request.PaymentRequest;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
+@CircuitBreaker(name = "external", fallbackMethod = "fallback")
 @FeignClient(name = "PAYMENT-SERVICE/payment")
 public interface PaymentService {
 
     @PostMapping
     public ResponseEntity<Long> doPayment(@RequestBody PaymentRequest paymentRequest);
+
+    default void fallback(Exception e) {
+        throw new CustomException("Payment service is down", "UNAVAILABLE", 500);
+    }
 }
